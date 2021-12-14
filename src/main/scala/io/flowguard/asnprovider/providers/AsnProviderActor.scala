b12fb369d2d81aadc32f965a22909782623eb0f3
Jakub Pravda <jakub.pravda@comsource.cz>
2021-12-14 22:47:22 +0100

    using log support instead actor logging + one time asn db loader

--- src/main/scala/io/flowguard/asnprovider/providers/AsnProviderActor.scala
+++ src/main/scala/io/flowguard/asnprovider/providers/AsnProviderActor.scala
@@ -4,6 +4,7 @@ import akka.actor.{Actor, ActorLogging, Props, Timers}
 import com.comcast.ip4s.IpAddress
 import io.flowguard.asnprovider.models.{AsnDatabase, AsnRecord}
 import Implicits.fromConfig
+import wvlet.log.LogSupport
 
 import scala.concurrent.duration._
 import scala.language.postfixOps
@@ -14,27 +15,28 @@ import scala.language.postfixOps
  * @param asnProvider ASN provider
  */
 class AsnProviderActor(refreshRate: Option[FiniteDuration])(implicit asnProvider: AsnProvider) extends Actor // TODO tests
-  with ActorLogging // TODO logging library
+  with LogSupport
   with Timers {
   import AsnProviderActor._
 
   refreshRate match {
     case None =>
-      // load once
-      log.debug("ASN database refresh rate => only at startup")
-      timers.startSingleTimer(TimerKey, RefreshAsnDatabase, 1.seconds)
     case Some(interval) =>
       // load per interval
-      log.debug(s"ASN database refresh rate => ${interval.toString()}")
+      logger.info(s"ASN database refresh rate => ${interval.toString()}")
       timers.startTimerWithFixedDelay(TimerKey, RefreshAsnDatabase, interval)
   }
 
-  override def receive: Receive = search(AsnDatabase.empty())
+  override def receive: Receive = {
+    logger.info("ASN database -> loading at startup")
+    self ! RefreshAsnDatabase
+    search(AsnDatabase.empty())
+  }
 
   def search(asnDatabase: AsnDatabase): Receive = {
 
     case RefreshAsnDatabase =>
-      log.info("ASN database refresh request")
+      logger.info("ASN database refresh request")
       context.become(search(asnProvider.load))
 
     case AsnNumRequest(ipAddress) =>
